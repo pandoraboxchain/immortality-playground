48a063d06884f04041a9e2264f34d1fd2ab4c7f0
ad_astra <olga.the.dark@gmail.com>
2018-11-28 02:00:18 +0300

    bracket fix

--- backend/talking_agent.py
+++ backend/talking_agent.py
@@ -17,7 +17,7 @@ class TelegramAgent(Agent):
 
     def observe(self, msg):
         current_message = TelegramAgent.message_queue.pop()
-        msg_text = display_messages(text=display_messages([msg], prettify=True, ignore_fields='text_candidates')
+        msg_text = display_messages(text=display_messages([msg], prettify=True, ignore_fields='text_candidates'))
         self.bot.send_message(chat_id=current_message.chat_id,
                               text=msg_text.replace("[Seq2Seq]:", ""))
 
